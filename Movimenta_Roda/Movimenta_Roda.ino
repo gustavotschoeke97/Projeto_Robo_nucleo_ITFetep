ca96402d816aa17767ce0605e31626d2103a3a35
gustavotschoeke97 <gustavotschoeke@gmail.com>
2019-05-10 19:02:43 -0300

    10/05/2019

--- Movimenta_Roda/Movimenta_Roda.ino
+++ Movimenta_Roda/Movimenta_Roda.ino
@@ -12,7 +12,7 @@
 #define PINO_REVERSO_DIR 7
 #define PINO_PWM_DIR     9
 #define PWM_DIR          83
-#define PWM_ESQ          70
+#define PWM_ESQ          90
 #define PWM_DIR_REV      83
 #define PWM_ESQ_REV      45
 
@@ -22,8 +22,7 @@ int  dir_esq, dir_dir, pwd_control_esq, pwd_control_esq_rev, pwd_control_dir, pw
 int  rpm_esq, rpm_dir;
 bool freio       = false;
 bool lib_control = false;
-Thread        t1 = Thread();
-Thread        t2 = Thread();
+Thread t[4];
 
 
 void setup() {
@@ -36,47 +35,76 @@ void setup() {
   pinMode(PINO_PWM_DIR    , OUTPUT);
   pinMode(PINO_REVERSO_DIR, OUTPUT);
   pinMode(PINO_FREIO_DIR  , OUTPUT);
-  t1.onRun(controleRPM);
-  t1.setInterval(50);
-  t2.onRun(controleRPM);
-  t2.setInterval(50);
+  
+  t[0].onRun(controleRPM);
+  t[0].setInterval(50);
+  t[1].onRun(controleRPM);
+  t[1].setInterval(50);
+  t[2].onRun(FrenteDir);
+  t[2].setInterval(75);
+  t[3].onRun(FrenteEsq);
+  t[3].setInterval(50);
 }
 
 void loop() {
-  
+
   if (lib_control == true) {
     if (wheel == "D") {
-      t1.run();
+      t[0].run();
     }
     else {
-      t2.run();
+      t[1].run();
     }
   }
-  
+
   if (Serial.available() > 0) {
     read_value = char(Serial.read());
     //Serial.println(read_value);
   }
   switch (read_value) {
     case '1': // girar para frente
-      pwd_control_esq     = PWM_ESQ;        
-      pwd_control_dir     = PWM_DIR;       
+      pwd_control_esq     = 332;
+      pwd_control_dir     = 70;
       freio = false;
       digitalWrite(PINO_FREIO, 1);
       digitalWrite(PINO_FREIO_DIR, 1);
       delay(300);
       digitalWrite(PINO_REVERSO, LOW);
       digitalWrite(PINO_REVERSO_DIR, LOW);
-      delay(300);
-      analogWrite(PINO_PWM, pwd_control_esq);
-      analogWrite(PINO_PWM_DIR, pwd_control_dir);
+      delay(500);
+      t[2].run();
+      t[3].run();
+      
+
+      /*for(int i = 0;i< 90;i++){
+          if(pwd_control_dir == PWM_DIR ){
+            continue;
+          }
+
+          else if( pwd_control_dir  < PWM_DIR ){
+            analogWrite(PINO_PWM_DIR, pwd_control_dir++);
+          }
+
+          if(pwd_control_esq == PWM_ESQ ){
+            continue;
+          }
+          else if( pwd_control_esq  < PWM_ESQ ){
+            analogWrite(PINO_PWM, pwd_control_esq+=6);
+          }
+
+          if(pwd_control_dir >= PWM_DIR && pwd_control_esq  >= PWM_ESQ){
+            break;
+          }
+          delay(70);
+        }*/
+
       lib_control = true;
       read_value = 0;
       break;
-      
+
     case '2': // girar para tras
-      pwd_control_esq_rev = PWM_ESQ_REV;  
-      pwd_control_dir_rev = PWM_DIR_REV; 
+      pwd_control_esq_rev = 74;
+      pwd_control_dir_rev = 46;
       freio = false;
       digitalWrite(PINO_FREIO, 1);
       digitalWrite(PINO_FREIO_DIR, 1);
@@ -89,29 +117,32 @@ void loop() {
       lib_control = true;
       read_value = 0;
       break;
-      
+
     case '3': // freiar tudo
       freio = true;
       read_value = Movimenta(freio);
       digitalWrite(PINO_FREIO, 0);
       digitalWrite(PINO_FREIO_DIR, 0);
-      delay(500);
+      while (rpm_dir > 0 && rpm_esq > 0) {
+        Serial.println("ta no while");
+      }
+      Serial.println("Saiu");
       digitalWrite(PINO_FREIO, 1);
       digitalWrite(PINO_FREIO_DIR, 1);
       lib_control = false;
-      
+      read_value = 0;
       break;
-      
-     case '4':// virar esquerda
+
+    case '4':// virar esquerda
       freio = true;
       read_value = Movimenta(freio);
       digitalWrite(PINO_FREIO, 1);
       digitalWrite(PINO_FREIO_DIR, 1);
-      analogWrite(PINO_PWM,pwd_control_esq); 
+      analogWrite(PINO_PWM, pwd_control_esq);
       lib_control = false;
       break;
-      
-      case '5': // virar direita
+
+    case '5': // virar direita
       freio = true;
       read_value = Movimenta(freio);
       digitalWrite(PINO_FREIO, 1);
@@ -139,7 +170,7 @@ void receiveEvent(int howMany) {
   }
   char c[sizeof(Receive_Data) + 1];
   Receive_Data.toCharArray(c, sizeof(c));
-  
+
   Serial.println(c);
   char* token = strtok(c, "|");
   int i = 0;
@@ -196,7 +227,7 @@ void controleRPM() {
     if (dir_esq == 0) {
       if (rpm_esq < 50 ) {
         analogWrite(PINO_PWM, pwd_control_esq_rev++);
-        delay(100);
+        delay(200);
       }
       else if (rpm_esq > 50 ) {
         analogWrite(PINO_PWM, pwd_control_esq_rev--);
@@ -214,4 +245,36 @@ void controleRPM() {
       }
     }
   }
+}
+
+
+void FrenteDir() {
+  for (int i = 0; i <= PWM_DIR; i++) {
+    if (pwd_control_dir >= PWM_DIR) {
+      break;
+    }
+    else {
+      analogWrite(PINO_PWM_DIR, pwd_control_dir++);
+    }
+    delay(75);
+
+  }
+
+  t[2].enabled = false;
+
+}
+
+void FrenteEsq() {
+  for (int i = 0; i <= PWM_ESQ; i++) {
+    if (pwd_control_esq >= PWM_ESQ) {
+      break;
+    }
+    else {
+      analogWrite(PINO_PWM, pwd_control_esq++);
+    }
+
+    delay(1);
+  }
+
+  t[3].enabled = false;
 }
