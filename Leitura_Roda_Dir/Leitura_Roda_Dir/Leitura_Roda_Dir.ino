db4ba118d150f1e0df4125acaea69f39a7bfbe43
gustavotschoeke97 <gustavotschoeke@gmail.com>
2019-06-13 21:53:45 -0300

    Update Leitura_Roda_Dir.ino

--- Leitura_Roda_Dir/Leitura_Roda_Dir/Leitura_Roda_Dir.ino
+++ Leitura_Roda_Dir/Leitura_Roda_Dir/Leitura_Roda_Dir.ino
@@ -9,21 +9,15 @@ void setup() {
   Serial.begin(9600);
   pinMode(PIN_2,  INPUT);
 }
-
 void loop() {
-
-
   if (duration = pulseIn(PIN_2, HIGH) > 0 ) {
     dir = digitalRead(DIRECAO);
     float valor = (duration * 2 * 15) / 1000000.0;
-
     valor = 1 / valor;
     int RPM = valor * 60; // multiplica por 60 para obter o valor do 
-
     String str = "D|" + String(dir) + "|" + String(RPM); //roda Esq/Dir|sentido da roda|RPM
     char c[10];
     str.toCharArray(c, 10);//// necessario converter para um char array antes de enviar
-
     Wire.beginTransmission(8); // transmite para o dispositvo de endereco 0x08
     Wire.write(c);
     Wire.endTransmission();
